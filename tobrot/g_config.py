9ec7c13f95c2960a683ad4af742f031da74d2d5a
Sekhar <43923138+Sekharbobby@users.noreply.github.com>
2021-05-27 04:25:04 -0400

    Create g_config.py

---
+++ tobrot/g_config.py
@@ -0,0 +1,10 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+    TG_BOT_TOKEN= "1847667566:AAEbFdlcorjB-Grde1TEC55isxRMXkbzfSQ" #imp
+    APP_ID = 2487180 #imp
+    API_HASH = "6d1e2e3cedd826e781cbe36e3249fc5c" #imp
+    OWNER_ID = 555076579
+    AUTH_CHANNEL = -1001418345293
+    DESTINATION_FOLDER = ""
+    RCLONE_CONFIG = ""
